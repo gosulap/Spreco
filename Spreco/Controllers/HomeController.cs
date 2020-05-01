d98dd81321643406278bf0760a5a66fe7d3bdd6b
Pradhit Gosula <pradhitg@gmail.com>
2020-05-01 15:27:00 -0400

    auth in

--- Spreco/Controllers/HomeController.cs
+++ Spreco/Controllers/HomeController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using Spreco.Models;
 
 namespace Spreco.Controllers
@@ -13,9 +16,12 @@ namespace Spreco.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IHttpClientFactory _clientFactory; 
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
         {
             _logger = logger;
+            _clientFactory = clientFactory;
         }
 
         public IActionResult Index()
@@ -23,6 +29,7 @@ namespace Spreco.Controllers
             string clientid = Environment.GetEnvironmentVariable("sprecoid");
             string url = "https://accounts.spotify.com/authorize?client_id=" +
                           clientid +
+                          "&response_type=code"+
                           "&redirect_uri=https://localhost:44336/home/callback" +
                           "&scope=user-read-private%20user-read-email" +
                           "&response_type=token" +
@@ -33,8 +40,41 @@ namespace Spreco.Controllers
             return View();
         }
         
-        public IActionResult Callback()
+
+        public async Task<IActionResult> CallbackAsync(string code)
         {
+            // things are the # are not even sent to the server so we cant see it here
+
+            // make a post request to get access and refresh tokens 
+
+            var client = _clientFactory.CreateClient(); 
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("sprecoid") + ":" + Environment.GetEnvironmentVariable("sprecosecret"))));
+
+            FormUrlEncodedContent formContent = new FormUrlEncodedContent(new[]
+            {
+                        new KeyValuePair<string, string>("code", code),
+                        new KeyValuePair<string, string>("redirect_uri", "https://localhost:44336/home/callback"),
+                        new KeyValuePair<string, string>("grant_type", "authorization_code"),
+             });
+
+            var response = client.PostAsync("https://accounts.spotify.com/api/token", formContent).Result;
+
+            var responseContent = response.Content;
+            string responseString = responseContent.ReadAsStringAsync().Result;
+
+            // access and refresh tokens are in this responsestring 
+            var json = JObject.Parse(responseString); 
+
+            string access_token = (string) json["access_token"];
+            string refresh_token = (string)json["refresh_token"];
+            string expires_in = (string)json["expires_in"];
+
+            Console.WriteLine(access_token);
+            Console.WriteLine(refresh_token);
+            Console.WriteLine(expires_in); 
+
+            // now we can use the api and get some actual information 
+
             return View(); 
         }
         public IActionResult Privacy()
