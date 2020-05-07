d17877422c72bc192c30bf6c5e098ad365bddccd
Pradhit Gosula <pradhitg@gmail.com>
2020-05-07 18:23:34 -0400

    werid stuff that ill come back to

--- Spreco/wwwroot/js/callback.js
+++ Spreco/wwwroot/js/callback.js
@@ -20,15 +20,31 @@ for (var i = 0; i < parallax_images.length; i++) {
     parallax_images[i].style.backgroundImage = "url(" + parallax_images[i].id + ")"; 
 }
 
+
 var tracks = document.getElementsByClassName("contents"); 
 var ids = []; 
 
 for (var i = 0; i < tracks.length; i++) {
     var temp = tracks[i].childNodes[3]; 
     for (var j = 1; j < 6; j += 2) {
+        console.log(temp.childNodes[j].childNodes[1].href); 
         ids.push(temp.childNodes[j].childNodes[1].href.split("https://open.spotify.com/track/")[1]); 
     }
 }
+
+
+/*
+var tracks = document.getElementsByClassName("row");
+var ids = [];
+
+for (var i = 0; i < tracks.length; i++) {
+    for (var j = 0; j < 3; j++) {
+        // not all rows have three things 
+        if (tracks[i].children[j]) {
+            ids.push(tracks[i].children[j].children[0].href.split("https://open.spotify.com/track/")[1]);
+        }
+    }
+} */ 
 
 var newhref = "/home/export?ids="; 
 
