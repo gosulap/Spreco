4a1ea85fc4cc3087d5cabf498ff378b6d4ae021d
Pradhit Gosula <pradhitg@gmail.com>
2020-05-07 15:20:16 -0400

    about in

--- Spreco/Controllers/HomeController.cs
+++ Spreco/Controllers/HomeController.cs
@@ -347,6 +347,11 @@ namespace Spreco.Controllers
             return View(); 
         }
 
+
+        public IActionResult About()
+        {
+            return View(); 
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
